ed5c2fb4aa3a88c049ded15984d4a7f847fef3b3
Filip95 <Filip95@users.noreply.github.com>
2021-12-23 12:51:53 +0100

    #18-Corrected isset condition to !empty in controller, added a button for movie reservation which is only shown when you click on a certain movie (#5)

--- modules/custom/reserve_movies/js/ShowMoviesByGenre.js
+++ modules/custom/reserve_movies/js/ShowMoviesByGenre.js
@@ -3,6 +3,7 @@ function ShowMoviesByGenre() {
         const movieGenre = localStorage.getItem("movieGenre"); 
         $('#genre').val(movieGenre);
     }
+
     $("#genre").on("change", function () {
         const genre = $(this).val();
         localStorage.setItem("movieGenre", genre);
@@ -13,6 +14,11 @@ function ShowMoviesByGenre() {
                 window.location.href = "./movie-reservation?genre=" + $("#genre").val();
             }
         });
+    });
+
+    $(".movie-class").on("click", function(){
+        $(this).toggleClass("movie-class-clicked");
+        $(this).find(".reserve-movies-button").first().toggleClass("show-button");
     });
  }
  ShowMoviesByGenre();
