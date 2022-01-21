3d94d2a53662819836a93f21ceab34ec5ee90967
Filip95 <filipsecic@gmail.com>
2022-01-21 16:15:18 +0100

    #20- Solved nested jquery issues

--- modules/custom/reserve_movies/js/ShowMoviesByGenre.js
+++ modules/custom/reserve_movies/js/ShowMoviesByGenre.js
@@ -1,4 +1,4 @@
-function ShowMoviesByGenre() {
+$(function ShowMoviesByGenre() {
     window.onload = function() {
         const movieGenre = localStorage.getItem("movieGenre"); 
         $('#genre').val(movieGenre);
@@ -7,33 +7,53 @@ function ShowMoviesByGenre() {
     $("#genre").on("change", function () {
         const genre = $(this).val();
         localStorage.setItem("movieGenre", genre);
-        $.ajax({
-            method: "GET",
+        $.get({
             url: "./movie-reservation?genre=" + $("#genre").val(),
             success: function () {
                 window.location.href = "./movie-reservation?genre=" + $("#genre").val();
             }
         });
     });
+});
 
-    $(".movie-class").on("click", function(){
-        const popUpID = "#popup-" + $(this).attr("id");
-        const reservationButtonID = "#reservation-button-" + $(this).attr("id");
-        const confirmReservationButton = "#confirm-reservation-button-" + $(this).attr("id");
-        const cancelPopupSelection = "#cancel-" + $(this).attr("id");
-        $(this).toggleClass("movie-class-clicked");
-        $(reservationButtonID).addClass("show-button");
-        $(reservationButtonID).on("click",function(){
-            $(popUpID).addClass("show-popup");
-        });
-        $(popUpID).on("change", function(){
-            $(cancelPopupSelection).show();
-            $(confirmReservationButton).addClass("show-confirmation");
-        });
-        $(cancelPopupSelection).on("click",function(){
-            $(confirmReservationButton).removeClass("show-confirmation");
-            $(popUpID).removeClass("show-popup");
-        });
-    });        
-}
- ShowMoviesByGenre();
+$(document).ready(function(){
+        $("div[id^='movie-']").each(function(){
+            const ID = parseInt(this.id.replace("movie-", ""),10);
+            const movieID = "#movie-" + ID;
+            const reserveButtonID = "#reservation-button-" + ID;
+            const popupID = "#popup-" + ID;
+            const cancelButtonID = "#cancel-" + ID;
+            const confirmButtonID = "#confirm-reservation-button-" + ID;
+            $(movieID).mouseenter(function () {
+                $(this).addClass("movie-class-clicked");
+                $(reserveButtonID).addClass("show-button");
+            });
+
+            $(movieID).mouseleave(function () {
+                $(this).removeClass("movie-class-clicked");
+                $(reserveButtonID).removeClass("show-button");
+            });
+             
+            $(reserveButtonID).on("click", function () {
+                $(popupID).toggleClass("show-popup");
+                if(!$(movieID).hasClass("movie-class-clicked") && $(popupID).hasClass("show-popup")){
+                    $(popupID).removeClass("show-popup");
+                }
+            });
+        
+            $(popupID).on("change", function () {
+                $(cancelButtonID).show();
+                $(confirmButtonID).addClass("show-confirmation");
+                if(!$(movieID).hasClass("movie-class-clicked") && $(confirmButtonID).hasClass("show-confirmation")){
+                    $(confirmButtonID).removeClass("show-confirmation");
+                    $(cancelButtonID).hide();
+                }
+            });
+        
+            $(cancelButtonID).on("click", function () {
+                $(confirmButtonID).removeClass("show-confirmation");
+                $(popupID).removeClass("show-popup");
+            });
+        });        
+});
+    
