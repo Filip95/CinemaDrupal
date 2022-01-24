416b396da40e50e36e5740ecea24a7116d476caf
Filip95 <Filip95@users.noreply.github.com>
2022-01-24 14:31:45 +0100

    Feature/#20 open popup on reserve movie button click (#6)
    * #20-Added a pop-up which appears when you click 'Reserve movie', which then offers to confirm reservation once you have selected a day. Yet to implement button and pop-up disappearance when user unselects the current movie
    
    * #20-fixed no newline in CSS
    
    * #20- Added a button that cancels popup and confirmation button
    
    * #20- Solved nested jquery issues
    
    * #20-Fixed "no newline" and changed function name to camel case
    
    * #20-newline

--- modules/custom/reserve_movies/js/ShowMoviesByGenre.js
+++ modules/custom/reserve_movies/js/ShowMoviesByGenre.js
@@ -1,4 +1,4 @@
-function ShowMoviesByGenre() {
+$(function showMoviesByGenre() {
     window.onload = function() {
         const movieGenre = localStorage.getItem("movieGenre"); 
         $('#genre').val(movieGenre);
@@ -7,18 +7,53 @@ function ShowMoviesByGenre() {
     $("#genre").on("change", function () {
         const genre = $(this).val();
         localStorage.setItem("movieGenre", genre);
-        $.ajax({
-            method: "GET",
+        $.get({
             url: "./movie-reservation?genre=" + $("#genre").val(),
             success: function () {
                 window.location.href = "./movie-reservation?genre=" + $("#genre").val();
             }
         });
     });
+});
 
-    $(".movie-class").on("click", function(){
-        $(this).toggleClass("movie-class-clicked");
-        $(this).find(".reserve-movies-button").first().toggleClass("show-button");
-    });
- }
- ShowMoviesByGenre();
+$(document).ready(function(){
+        $("div[id^='movie-']").each(function(){
+            const ID = parseInt(this.id.replace("movie-", ""),10);
+            const movieID = "#movie-" + ID;
+            const reserveButtonID = "#reservation-button-" + ID;
+            const popupID = "#popup-" + ID;
+            const cancelButtonID = "#cancel-" + ID;
+            const confirmButtonID = "#confirm-reservation-button-" + ID;
+            $(movieID).mouseenter(function () {
+                $(this).addClass("movie-class-clicked");
+                $(reserveButtonID).addClass("show-button");
+            });
+
+            $(movieID).mouseleave(function () {
+                $(this).removeClass("movie-class-clicked");
+                $(reserveButtonID).removeClass("show-button");
+            });
+             
+            $(reserveButtonID).on("click", function () {
+                $(popupID).toggleClass("show-popup");
+                if(!$(movieID).hasClass("movie-class-clicked") && $(popupID).hasClass("show-popup")){
+                    $(popupID).removeClass("show-popup");
+                }
+            });
+        
+            $(popupID).on("change", function () {
+                $(cancelButtonID).show();
+                $(confirmButtonID).addClass("show-confirmation");
+                if(!$(movieID).hasClass("movie-class-clicked") && $(confirmButtonID).hasClass("show-confirmation")){
+                    $(confirmButtonID).removeClass("show-confirmation");
+                    $(cancelButtonID).hide();
+                }
+            });
+        
+            $(cancelButtonID).on("click", function () {
+                $(confirmButtonID).removeClass("show-confirmation");
+                $(popupID).removeClass("show-popup");
+            });
+        });        
+});
+
